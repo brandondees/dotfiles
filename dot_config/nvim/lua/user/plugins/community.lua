9ba3b3a24490b8c31c0f9fcd505c7b19e251972a
Brandon Dees <dees@pm.me>
2023-10-31 13:10:23 -0500

    Update .config/nvim/lua/user/plugins/community.lua

--- dot_config/nvim/lua/user/plugins/community.lua
+++ dot_config/nvim/lua/user/plugins/community.lua
@@ -6,4 +6,22 @@ return {
 
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity.completion.copilot-lua" },
+  { -- further customize the options set by the community
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = {
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<C-.>",
+          prev = "<C-,>",
+          dismiss = "<C/>",
+        },
+      },
+    },
+  },
 }
